e48bb2d1038fd160565bca069013af9f1c8e05e3
nfzmhn <zidninurfauzi2203@gmail.com>
2024-10-11 19:28:49 +0700

    update commite spinner

--- app/src/main/java/pam/lat/droidcafee/OrderActivity.kt
+++ app/src/main/java/pam/lat/droidcafee/OrderActivity.kt
@@ -2,16 +2,46 @@ package pam.lat.droidcafee
 
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.RadioButton
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class OrderActivity : AppCompatActivity() {
+    private lateinit var spinnerCity: Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_order)
+
+        val spinnerCity: Spinner = findViewById(R.id.spinner_city)
+
+        val cities = arrayOf("Jakarta", "Bandung", "Cimahi", "Kabupaten Bandung")
+
+        val adapter: ArrayAdapter<String> = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            cities
+        )
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerCity.adapter = adapter
+
+        spinnerCity.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                val selectedCity = parent.getItemAtPosition(position).toString()
+                Toast.makeText(this@OrderActivity, "Selected: $selectedCity", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                //Nothing
+            }
+        }
+
     }
     fun onRadioButtonClicked(view: View) {
 
@@ -39,4 +69,6 @@ class OrderActivity : AppCompatActivity() {
             Toast.LENGTH_SHORT
         ).show()
     }
+
+
 }
